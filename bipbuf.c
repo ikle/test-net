2536c9d8b48f6c3f342ac24685f72377befbc78d
Alexei A. Smekalkine <ikle@ikle.ru>
2015-02-11 19:09:45 +0300

    bipbuf: import bipartite buffer implementation

---
+++ bipbuf.c
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2014, 2015 Alexei A. Smekalkine <ikle at ikle.ru>.
+ * All rights reserved.
+ *
+ * This program is free software.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE DEVELOPERS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include "bipbuf.h"
+
+struct bipbuf {
+	char *head, *tail, *a_head, *a_tail, *b_tail;
+	int is_a_active;
+};
+
+struct bipbuf *bipbuf_alloc (size_t size)
+{
+	struct bipbuf *b;
+
+	if ((b = malloc (sizeof (*b))) == NULL)
+		goto no_object;
+
+	if ((b->head = malloc (size)) == NULL)
+		goto no_buffer;
+
+	b->tail = b->head + size;
+	b->a_head = b->a_tail = b->b_tail = b->head;
+	b->is_a_active = 1;
+	return b;
+
+no_buffer:
+	free (b);
+no_object:
+	return NULL;
+}
+
+void bipbuf_free (struct bipbuf *b)
+{
+	if (b == NULL)
+		return;
+
+	free (b->head);
+	free (b);
+}
+
+int bipbuf_is_empty (struct bipbuf *b)
+{
+	return b->a_head == b->a_tail;
+}
+
+void *bipbuf_reserve (struct bipbuf *b, size_t *size)
+{
+	if (!b->is_a_active) {
+		*size = b->a_head - b->b_tail;
+		return b->b_tail;
+	}
+
+	*size = b->tail - b->a_tail;
+	return b->a_tail;
+}
+
+void bipbuf_commit (struct bipbuf *b, size_t size)
+{
+	size_t a_avail, b_avail;
+
+	if (!b->is_a_active) {
+		b->b_tail += size;
+		assert (b->b_tail <= b->a_head);
+		return;
+	}
+
+	b->a_tail += size;
+	assert (b->a_tail <= b->tail);
+
+	a_avail = b->tail - b->a_tail;
+	b_avail = b->a_head - b->head;
+
+	if (b_avail > a_avail)
+		b->is_a_active = 0;
+}
+
+void *bipbuf_request (struct bipbuf *b, size_t *size)
+{
+	*size = b->a_tail - b->a_head;
+	return b->a_head;
+}
+
+void bipbuf_release (struct bipbuf *b, size_t size)
+{
+	b->a_head += size;
+	assert (b->a_head <= b->a_tail);
+
+	if (b->a_head < b->a_tail)  /* is A region not empty? */
+		return;
+
+	/* move B region to A */
+	b->a_head = b->head;
+	b->a_tail = b->b_tail;
+	b->b_tail = b->head;
+	b->is_a_active = 1;
+}
---
+++ bipbuf.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2014, 2015 Alexei A. Smekalkine <ikle at ikle.ru>.
+ * All rights reserved.
+ *
+ * This program is free software.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE DEVELOPERS ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE DEVELOPERS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _BIPBUF_H
+#define _BIPBUF_H  1
+
+#include <stddef.h>
+
+struct bipbuf *bipbuf_alloc (size_t size);
+void bipbuf_free (struct bipbuf *b);
+
+int bipbuf_is_empty (struct bipbuf *b);
+
+/*
+ * 1. Reserve region for writing, returns data pointer and room
+ *    available in buffer for writing.
+ * 2. Write some data into region.
+ * 3. Commit written data, size should be less or equal reserved region
+ *    size.
+ */
+void *bipbuf_reserve (struct bipbuf *b, size_t *size);
+void  bipbuf_commit  (struct bipbuf *b, size_t  size);
+
+/*
+ * 1. Request region for reading, returns data pointer and room
+ *    available in buffer for reading.
+ * 2. Read same data from requested region.
+ * 3. Release readed data, size should be less or equal requested
+ *    region size.
+ */
+void *bipbuf_request (struct bipbuf *b, size_t *size);
+void  bipbuf_release (struct bipbuf *b, size_t  size);
+
+#endif  /* _BIPBUF_H */
